97273cbe46985ca4fd877c237e5ef85ef4152a42
Keith Bartolotta <mashidin@hotmail.com>
2012-05-06 20:00:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PeaksSite::Application.config.secret_token = 'ac1b8425b6a4d698cea0ccbeaaab43017c504ccc839d712c73184967310b45f3fde637271606fe256b6b1a5033e1791aaca52b337988788588879a7a9539d126'
